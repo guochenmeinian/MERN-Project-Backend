d4f7413f3bd9ac890c64177ec12c64c5296ee137
guochenmeinian <guochenmeinian@outlook.com>
2022-07-11 16:41:49 -0400

    finished

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,10 +1,7 @@
-const { json } = require("express/lib/response");
-
 const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-
-router.post("/", (req,res)=>{
+router.post("/payment", (req,res)=>{
     stripe.charges.create({
         source: req.body.tokenId,
         amount: req.body.amount,
